0330a00512bc4d5e64f47e9c3fbf541b2665ac89
jeffreyroach94 <167149794+jeffreyroach94@users.noreply.github.com>
2024-04-24 13:16:30 -0500

    Add files via upload

--- receiver.ino
+++ receiver.ino
@@ -1,11 +1,14 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <FastLED.h>
 
 RF24 radio(7, 8); // CE, CSN
 
 const byte address[6] = "00001";
 const int LED_Pin = 6;
+
+CRGB leds[4];
 
 void setup() {
 
@@ -15,17 +18,34 @@ void setup() {
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
+  FastLED.addLeds<WS2812, LED_Pin, GRB>(leds, 4);
 }
 
 void loop() {
   if (radio.available()) {
     char text[32] = "";
     radio.read(&text, sizeof(text));
+    Serial.println(text);
     if (strcmp(text, "1") == 0) {
-      digitalWrite(LED_Pin, HIGH);
+      leds[0] = CRGB(255, 0, 0);
+      FastLED.show();
+      delay(500);  
+      leds[1] = CRGB(0, 255, 0);
+      FastLED.show();
+      delay(500);
+      leds[2] = CRGB(0, 0, 255);
+      FastLED.show();
+      delay(500);
+      leds[3] = CRGB(150, 0, 255);
+      FastLED.show();
     }
     else {
-      digitalWrite(LED_Pin, LOW);
+      leds[0] = CRGB(0, 0, 0);
+      leds[1] = CRGB(0, 0, 0);
+      leds[2] = CRGB(0, 0, 0);
+      leds[3] = CRGB(0, 0, 0);
+      FastLED.show();
     }
   }
+
 }
--- transmitter.ino
+++ transmitter.ino
@@ -10,25 +10,58 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <FastLED.h>
 
 RF24 radio(7, 8); // CE, CSN
 
 const byte address[6] = "00001";
-const char text[] = "1";
-const char text2[] = "0";
+const char text1[] = "0";
+const char text2[] = "1";
 
 void setup() {
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
   radio.stopListening();
+  Serial.begin(9600);
+  pinMode(2, INPUT);
 }
 
-void loop() {
+// CRGB leds[4];
 
-  radio.write(&text, sizeof(text));
-  delay(500);
+// void setup() {
 
-  radio.write(&text2, sizeof(text2));
-  delay(250);
+//   FastLED.addLeds<WS2812, 6, GRB>(leds, 4);
+
+// }
+
+void loop() {\
+  char text;
+
+  int switchState = digitalRead(2);
+  if (switchState == HIGH) {
+    Serial.println("HIGH");
+    radio.write(&text2, sizeof(text2));
+  } else {
+    Serial.println("LOW");
+    radio.write(&text1, sizeof(text1));
+  }
+
+  delay(100);
 }
+
+// void loop() {
+  
+//   leds[0] = CRGB(255, 0, 0);
+//   FastLED.show();
+//   delay(500);  
+//   leds[1] = CRGB(0, 255, 0);
+//   FastLED.show();
+//   delay(500);
+//   leds[2] = CRGB(0, 0, 255);
+//   FastLED.show();
+//   delay(500);
+//   leds[3] = CRGB(150, 0, 255);
+//   FastLED.show();
+//   delay(500);
+// }
